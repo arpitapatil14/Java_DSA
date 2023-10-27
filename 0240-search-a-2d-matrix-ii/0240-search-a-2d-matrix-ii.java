d4146619fe5b27d1ae944c94a64f863a99ce3337
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2023-10-27 14:42:30 +0530

    Time: 10 ms (15.34%), Space: 49.2 MB (29.3%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int row=0;row<matrix.length;row++){
+            for(int col=0;col<matrix[row].length;col++){
+                if(matrix[row][col]== target){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+}
