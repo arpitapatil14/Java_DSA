e0de4b4c0034549db51c958842efb08a1230dbba
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2023-11-02 23:27:55 +0530

    Time: 1 ms (76.57%), Space: 39.4 MB (66.25%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        return (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
+    }
+}
