5cac829ffb2e2ec23de6e41645cd8ddb612b29a2
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2023-11-11 22:19:34 +0530

    Time: 0 ms (100%), Space: 44.8 MB (46.66%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+
+        return -1; 
+    }
+}
