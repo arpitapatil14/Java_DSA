82054026d15b3f5fb961747f8e28854130fe1163
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2024-06-01 13:16:59 +0530

    Time: 1 ms (99.9%), Space: 45.8 MB (63.18%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int value=0;  
+        for(int i=0;i<nums.length;i++){
+            value=value^nums[i];
+        }   
+        return value;
+    }
+}
