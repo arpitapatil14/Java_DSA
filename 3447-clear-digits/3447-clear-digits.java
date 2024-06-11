531366674efec2df4240ccfb9abd32fe8b2b5d35
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2024-06-11 22:09:39 +0530

    Time: 1 ms (100%), Space: 42.1 MB (92.53%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder ans= new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                if(s.length()>0){
+                    ans.deleteCharAt(ans.length()-1);
+                }
+            }else{
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+        
+    }
+}
