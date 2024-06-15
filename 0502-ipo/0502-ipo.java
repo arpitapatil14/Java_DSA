0964bd86f76a1527faf894fd6063bfcca1e67169
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2024-06-15 22:33:41 +0530

    Time: 91 ms (27.27%), Space: 64.4 MB (12.48%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+
+        List<int[]> projects = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            projects.add(new int[]{capital[i], profits[i]});
+        }
+
+        projects.sort(Comparator.comparingInt(a -> a[0]));
+
+        PriorityQueue<Integer> maxProfitHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        int i = 0;
+
+        while (k-- > 0) {
+            while (i < n && projects.get(i)[0] <= w) {
+                maxProfitHeap.add(projects.get(i)[1]);
+                i++;
+            }
+            if (maxProfitHeap.isEmpty()) {
+                break;
+            }
+            w += maxProfitHeap.poll();
+        }
+        return w;
+    }
+}
