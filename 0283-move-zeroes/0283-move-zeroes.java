648a6f26062b926b440d5ada1cf9b2068b2c9a51
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2024-06-26 13:44:10 +0530

    Time: 2 ms (83.32%), Space: 45.7 MB (76.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[j]=nums[i];
-                if(i!=j){
-                nums[i]=0;
-            }
-            j++;
-            }
-            
-        }
         
+        int[] ans = new int[nums.length]; 
+        Arrays.fill(ans, 0); 
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                ans[j] = nums[i];
+                j++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = ans[i];
+        }
     }
 }
