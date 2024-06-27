d10dfcc6c337fae11f327b533a7722ff2eeefa7e
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2024-06-27 20:17:41 +0530

    Time: 0 ms (100%), Space: 40.7 MB (49.98%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==0){
+        if(n<=0){
             return false;
         }
-        if(n==1 || n>1 && (n & (n - 1)) == 0){
-            return true;
-        }
-
-        return false;
+        return (n&(n-1))==0;
         
     }
 }
