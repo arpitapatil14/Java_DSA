6cec15b71e8e567edcb5fa4ceef25eadb7e2220f
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2024-07-06 22:13:19 +0530

    Time: 0 ms (100%), Space: 40.4 MB (33.05%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int totalFullRounds = time / (n - 1);
+        int timeLeft = time % (n - 1);
+
+        if (totalFullRounds % 2 == 0) {
+            return timeLeft + 1;
+        } else {
+            return n - timeLeft;
+        }
+    }
+}
