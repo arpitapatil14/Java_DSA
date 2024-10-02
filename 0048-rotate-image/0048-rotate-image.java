7ac143ba1878836d748e517205acea74d9550d57
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2024-10-03 02:14:24 +0530

    Time: 0 ms (100%), Space: 42.3 MB (27.93%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,20 +1,28 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]= matrix[j][i];
-                matrix[j][i]=temp;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // Transpose 
+        for (int i = 0; i < m; i++) {
+            for (int j = i; j < n; j++) { 
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] = temp;
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        
+        for (int i = 0; i < m; i++) {
+            reverse(matrix[i]);
+        }
+    }
+    private void reverse(int[] mat) { //rev array(row)
+        int left = 0;
+        int right = mat.length - 1;
+        while (left < right) {
+            int temp = mat[left]; 
+            mat[left] = mat[right]; 
+            mat[right] = temp; 
+            left++;
+            right--;
+        }
     }
 }
