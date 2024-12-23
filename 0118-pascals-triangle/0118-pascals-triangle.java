23032fb042783e55a568bb6b5511f61012cb6504
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2024-12-23 21:59:13 +0530

    Time: 0 ms (100%), Space: 41.9 MB (59.07%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,23 +1,23 @@
-public class Solution {
-    public static int ncr(int n, int r) {
-        long ans = 1;
-        for (int i = 0; i < r; i++) {
-            ans *= (n - i);
-            ans /= (i + 1);
+class Solution {
+    public List<Integer> ncr(int n){
+        long ans=1;
+        List<Integer> temp= new ArrayList<>();
+        temp.add(1);
+        for(int i=1;i<n;i++){
+            ans=ans*(n-i);
+            ans=ans/i;
+            temp.add((int)ans);
         }
-        return (int) ans;
+        return temp;
+
     }
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
-        
-        for (int row = 0; row < numRows; row++) {
-            List<Integer> temp = new ArrayList<Integer>();
-            for (int col = 0; col <= row; col++) {
-                temp.add(ncr(row, col)); 
-            }
-            triangle.add(temp);
+        List<List<Integer>> finalans= new ArrayList<>();
+        for (int row = 1; row <= numRows; row++) {
+            finalans.add(ncr(row));
         }
-        return triangle;
+        return finalans;
+        
     }
-
 }
+
