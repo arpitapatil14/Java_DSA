4b850a79e4fdbd0746df9ecc53b126d742e7b5fc
Arpita Patil <103423006+arpitapatil14@users.noreply.github.com>
2024-12-29 00:27:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,21 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
-        if (n == 0) {
+        int n=intervals.length;
+        if(n==0){
             return new int[0][0];
         }
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int[][] ans = new int[n][2];
-        int idx = 0;
-        ans[idx] = intervals[0];
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] <= ans[idx][1]) {
-                ans[idx][1] = Math.max(ans[idx][1], intervals[i][1]);
-            } else {
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        int[][] ans= new int[n][2];
+        int idx=0;
+        ans[idx]=intervals[0];
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=ans[idx][1]){
+                ans[idx][1]= Math.max(ans[idx][i],intervals[i][1]);
+            }else{
                 idx++;
-                ans[idx] = intervals[i];
+                ans[idx]=intervals[i];
             }
         }
-        return Arrays.copyOf(ans, idx + 1);
+        return Arrays.copyOf(ans,idx+1);
+        
     }
 }
